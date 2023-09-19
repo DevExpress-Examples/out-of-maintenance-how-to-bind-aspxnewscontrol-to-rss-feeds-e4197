038e52139cc57acfbbdffe1d70b3125af0d7f71b
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-19 15:29:28 +0400

    Change example status to out of maintenance

--- CS/WebSite/Default.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Web;
-using System.Web.UI;
-
-public partial class _Default : System.Web.UI.Page
-{
- 
-}
--- VB/WebSite/Default.aspx.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
-Imports System.Web
-Imports System.Web.UI
-
-Partial Public Class _Default
-	Inherits System.Web.UI.Page
-
-End Class
